4231f0efbd19a9f1dab12babc664177b509c54e2
sheik gulfaan <sheikgulf01@gmail.com>
2024-07-24 07:12:38 +0530

    dup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ const Header = () => {
   return (
     <div className='bg-primary'>
       <div className='d-flex justify-content-between align-items-center container-lg'>
-          <h1><a href="/" className='text-light text-decoration-none py-5'>Net-Tv</a></h1>
+          <h1><a href="/" className='text-light text-decoration-none py-5'>Net-Tv</a></h1>v;sjdv'jivj
+          sljvhilgvlsfvh
       </div>
     </div>
   )
